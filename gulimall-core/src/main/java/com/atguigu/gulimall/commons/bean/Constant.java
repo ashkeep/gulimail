f9e4d99b13e94c3020900558d0daa3cd9653656d
ashkeep <1393067551@qq.com>
2021-01-28 11:40:39 +0800

    登录注册

--- gulimall-core/src/main/java/com/atguigu/gulimall/commons/bean/Constant.java
+++ gulimall-core/src/main/java/com/atguigu/gulimall/commons/bean/Constant.java
@@ -5,5 +5,13 @@ public class Constant {
     public static  final String ES_SPU_INDEX = "gulimall";
     public static  final String ES_SPU_TYPE = "spu";
 
+    public static final String CACHE_CATELOG = "cache:catelog:";
+
+    public static final String LOGIN_USER_PREFIX = "login:user:";
+
+    public static final Long LOGIN_USER_TIME_MINUTES = 30L;
+
+    public static final String CART_PREFIX = "cart:user:";
+
 
 }
